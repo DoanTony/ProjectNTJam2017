67d4672f5add8dd0100aa3d536098920e1b0aa52
Tony Doan <doantony90@gmail.com>
2017-01-22 10:15:42 -0500

    Lots of changes!

--- ProjectNTJam2017/Assets/Scripts/MagicBall.cs
+++ ProjectNTJam2017/Assets/Scripts/MagicBall.cs
@@ -12,7 +12,6 @@ public class MagicBall : MonoBehaviour
     RuneAction runeAction;
     public float VelMax;
     public float VelMin;
-    public bool RandomBallB = true;
     LevelManager levelManager;
     public bool inputAble = false;
 
@@ -24,7 +23,6 @@ public class MagicBall : MonoBehaviour
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
-        InvokeRepeating("RandomBall", 2.0f, 5f);
         IsBallMoving = false;
         selfRigidbody = GetComponent<Rigidbody>();
         BallDirectionHorizontal = Random.Range(-1, 2);
@@ -96,118 +94,114 @@ public class MagicBall : MonoBehaviour
     void OnTriggerStay(Collider other)
     {
         float hu = 1.5f;
-        Vector3 opposite = selfRigidbody.velocity * -1;
-        if(inputAble == true)
+        if(gameObject != null)
         {
-            if (other.tag == "Rune")
+            Vector3 opposite = selfRigidbody.velocity * -1;
+            if(inputAble == true)
             {
-                RandomBallB = false;
-                #region Player1
-                if (other.name == "RuneXDownP1" && runeAction.P1XDown == true)
+                if (other.tag == "Rune")
                 {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP1();
-                }
-                else if (other.name == "RuneXUpP1" && runeAction.P1XUp == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP1();
-                }
-                else if (other.name == "RuneYUpP1" && runeAction.P1YUp == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP1();
-                }
-                else if (other.name == "RuneYDownP1" && runeAction.P1YDown == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP1();
-                }
-                else if (other.name == "RuneBDownP1" && runeAction.P1BDown == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP1();
-                }
-                else if (other.name == "RuneBUpP1" && runeAction.P1BUp == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP1();
-                }
-                else if (other.name == "RuneAP1" && runeAction.P1A == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP1();
-                }
-                #endregion
+                    #region Player1
+                    if (other.name == "RuneXDownP1" && runeAction.P1XDown == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP1();
+                    }
+                    else if (other.name == "RuneXUpP1" && runeAction.P1XUp == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP1();
+                    }
+                    else if (other.name == "RuneYUpP1" && runeAction.P1YUp == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP1();
+                    }
+                    else if (other.name == "RuneYDownP1" && runeAction.P1YDown == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP1();
+                    }
+                    else if (other.name == "RuneBDownP1" && runeAction.P1BDown == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP1();
+                    }
+                    else if (other.name == "RuneBUpP1" && runeAction.P1BUp == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP1();
+                    }
+                    else if (other.name == "RuneAP1" && runeAction.P1A == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP1();
+                    }
+                    #endregion
 
-                #region Player2
-                if (other.name == "RuneXDownP2" && runeAction.P2XDown == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP2();
-                }
-                else if (other.name == "RuneXUpP2" && runeAction.P2XUp == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP2();
-                }
-                else if (other.name == "RuneYUpP2" && runeAction.P2YUp == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP2();
-                }
-                else if (other.name == "RuneYDownP2" && runeAction.P2YDown == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP2();
-                }
-                else if (other.name == "RuneBDownP2" && runeAction.P2BDown == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP2();
-                }
-                else if (other.name == "RuneBUpP2" && runeAction.P2BUp == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP2();
-                }
-                else if (other.name == "RuneAP2" && runeAction.P2A == true)
-                {
-                    func(opposite, hu);
-                    runeAction.ComboFuncP2();
-                }
-                #endregion
+                    #region Player2
+                    if (other.name == "RuneXDownP2" && runeAction.P2XDown == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP2();
+                    }
+                    else if (other.name == "RuneXUpP2" && runeAction.P2XUp == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP2();
+                    }
+                    else if (other.name == "RuneYUpP2" && runeAction.P2YUp == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP2();
+                    }
+                    else if (other.name == "RuneYDownP2" && runeAction.P2YDown == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP2();
+                    }
+                    else if (other.name == "RuneBDownP2" && runeAction.P2BDown == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP2();
+                    }
+                    else if (other.name == "RuneBUpP2" && runeAction.P2BUp == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP2();
+                    }
+                    else if (other.name == "RuneAP2" && runeAction.P2A == true)
+                    {
+                        func(opposite, hu);
+                        runeAction.ComboFuncP2();
+                    }
+                    #endregion
 
-                //Debug.Log("Velocity : " + selfRigidbody.velocity);
-            }
-            else if (other.tag == "GoalP1")
-            {
-                gameManager.PlayBlueScore();
-                StaticData.Player2Score++;
-                Destroy(gameObject);
-                if (levelManager.Player1Point == levelManager.WinPoint)
+                    //Debug.Log("Velocity : " + selfRigidbody.velocity);
+                }
+                else if (other.tag == "GoalP1")
                 {
-                    //Player 1 WIN
+                    gameManager.PlayBlueScore();
+                    StaticData.Player2Score++;
+                    Destroy(gameObject);
+                    if (levelManager.Player1Point == levelManager.WinPoint)
+                    {
+                        //Player 1 WIN
+                    }
+                }
+                else if (other.tag == "GoalP2")
+                {
+                    gameManager.PlayRedScore();
+                    StaticData.Player1Score++;
+                    Destroy(gameObject);
+                    if (levelManager.Player2Point == levelManager.WinPoint)
+                    {
+                        //Player 2 WIN
+                    }
                 }
             }
-            else if (other.tag == "GoalP2")
-            {
-                gameManager.PlayRedScore();
-                StaticData.Player1Score++;
-                Destroy(gameObject);
-                if (levelManager.Player2Point == levelManager.WinPoint)
-                {
-                    //Player 2 WIN
-                }
-            }
+
         }
-        
-
-    }
-
-    void OnColliderEnter(Collision col)
-    {
-        RandomBallB = false;
     }
 
     public void MoveBall()
