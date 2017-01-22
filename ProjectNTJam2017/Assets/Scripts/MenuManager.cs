eb83cabf83fb9e3df4f09184de29622c78deef6c
Haruyuki46 <francis_ruel514@hotmail.com>
2017-01-22 03:13:29 -0500

    TTTTTTYYYYYYY

---
+++ ProjectNTJam2017/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    EventSystem eventSystem;
+    Vector3 postionPlay;
+    Vector3 positionOption;
+    Vector3 positionQuit;
+    Animator anim; 
+    bool optionOpen = false;
+
+   public  GameObject option;
+
+	// Use this for initialization
+	void Start () {
+        positionOption = new Vector3(0,188,0);
+        postionPlay = new Vector3(0, 123, 0);
+        positionQuit = new Vector3(0, 60, 0);
+        anim = GameObject.Find("Canvas").GetComponent<Animator>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Option()
+    {
+        if (optionOpen == false)
+        {
+            anim.Play("Option");
+            optionOpen = true;
+            option.active = true;
+        }
+        else
+        {
+            anim.Play("OptionOut");
+            optionOpen = false;
+            option.active = false;
+        }
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void play()
+    {
+
+    }
+
+
+}
---
+++ ProjectNTJam2017/Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenManager : MonoBehaviour
+{
+
+    public string annee;
+    public string mois;
+    public string jour;
+    public Text dateText;
+
+    // Use this for initialization
+    void Start()
+    {
+        #region Date
+        jour = System.DateTime.Now.Date.Day.ToString();
+        annee = System.DateTime.Now.Date.Year.ToString();
+        switch (System.DateTime.Now.Date.Month)
+        {
+            case 1:
+                mois = "January";
+                break;
+            case 2:
+                mois = "February";
+                break;
+            case 3:
+                mois = "March";
+                break;
+            case 4:
+                mois = "April";
+                break;
+            case 5:
+                mois = "May";
+                break;
+            case 6:
+                mois = "June";
+                break;
+            case 7:
+                mois = "July";
+                break;
+            case 8:
+                mois = "August";
+                break;
+            case 9:
+                mois = "September ";
+                break;
+            case 10:
+                mois = "October";
+                break;
+            case 11:
+                mois = "November";
+                break;
+            case 12:
+                mois = "December";
+                break;
+
+        }
+        #endregion
+        dateText.text = (jour + " " + mois + " " + annee);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.anyKey)
+        {
+            //PlaySound when player press button
+            Application.LoadLevel("MenuScreen");
+        }
+
+    }
+
+}
+
